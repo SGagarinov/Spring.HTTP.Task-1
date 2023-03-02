e72be8a01370078d2d16a5f9853bcfb8ae473f9d
Abriial <codjumper@mail.ru>
2023-03-02 14:58:45 +0300

    Task-2:Handlers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import classes.Handler;
+import classes.Request;
 import classes.Server;
+import classes.ThreadPool;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -10,6 +13,28 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws IOException {
         Server server = new Server(9999);
+
+        server.addHandler("GET", "/index.html", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                try {
+                    ThreadPool.getResponse(request, responseStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        server.addHandler("GET", "/forms.html", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                try {
+                    ThreadPool.getResponse(request, responseStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         server.run();
     }
 }
